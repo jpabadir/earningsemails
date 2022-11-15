1f0b562b68b8477b45f794caf35ce2bf0cf538c7
FabienThich <fabienthich30@gmail.com>
2022-11-15 16:00:59 -0500

    Get Input Data

--- src/components/NetIncome/netincome.js
+++ src/components/NetIncome/netincome.js
@@ -1,34 +1,42 @@
 import React from "react";
-import CIK from './CIK.json';
+import CIK from "./CIK.json";
+import { useState } from "react";
 
 function NetIncomeLoss() {
-    console.log(CIK)
-    return (
-      <div>
-        <div>
-          {/* {INPUT BOX} */}
-          <input name="searchTicker" type='text' id="searchTicker" />
-          <input type='button' value='Search'></input>
-        </div>
-        <div>
-          {/* {{CIK.fields[1]}} */}
-           { CIK.fields.map( list => {
-              return(
-                  <p key={list}>{ list }</p>
-              )
-          })}
-        </div>
-      </div>
-    );
+  const [ticker, tickerValue] = useState("");
+  function clickToGetTicker() {
+    console.log(ticker);
   }
-  
-  export default NetIncomeLoss;
+  //console.log(CIK)
+  return (
+    <div>
+      <div>
+        {/* {INPUT BOX} */}
+        <input
+          name="searchTicker"
+          type="text"
+          id="searchTicker"
+          onChange={(value) => {
+            tickerValue(value.target.value);
+          }}
+        />
+        <input type="button" value="Search" onClick={clickToGetTicker} />
+      </div>
+      <div>
+        {/* {{CIK.fields[1]}} */}
+        {CIK.fields.map((list) => {
+          return <p key={list}>{list}</p>;
+        })}
+      </div>
+    </div>
+  );
+}
 
-
+export default NetIncomeLoss;
 
 // Take ticker input from user then find the corresponding CIK number.
 // Fill the left of the CIK number with 0's until there are a total of 10 digits. (CIK numbers must be 10 digits including 0's)
 // Plug the CIK number into https://data.sec.gov/api/xbrl/companyconcept/CIK##########/us-gaap/NetIncomeLoss.json.
-// Send a GET request. 
+// Send a GET request.
 // Read JSON with JS and get the value of the "val" key using document.getElementById.
 // Print the values.
