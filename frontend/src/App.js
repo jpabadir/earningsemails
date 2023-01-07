823be1db20bc60f1798181f67366aaea3f48badf
JP Abadir <jpabadir@gmail.com>
2023-01-07 10:10:06 -0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,11 @@
 import "./App.css";
-import NetIncomeLoss from "./components/NetIncome/netincome";
-import { Component } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <NetIncomeLoss />
-          <div>hey!</div>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Astraset</h1>
+    </div>
+  );
 }
 
 export default App;
