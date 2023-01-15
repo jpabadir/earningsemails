bc29e0d1998eb82ebefa7436c84dcd1e3f6ca7b5
JP Abadir <jpabadir@gmail.com>
2023-01-15 12:19:19 -0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,10 +32,10 @@ function App() {
   }
 
   function submitForm() {
-    addDoc(collection(db, "form-submissions"), {
-      email: emailInput,
-      tickers: tickers
-    });
+    // addDoc(collection(db, "form-submissions"), {
+    //   email: emailInput,
+    //   tickers: tickers
+    // });
 
     setEmailInput("");
     setTickers([]);
@@ -50,29 +50,40 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <ToastContainer />
-      <h1>Welcome to earningsemails.com!</h1>
-      <h2>What is it?</h2>
-      <p>I was sick of missing earnings for stocks I follow. So I created this site.</p>
-      <p>It will autmatically send you 2 emails for each earnings report:</p>
-      <h2>How does it work?</h2>
-      <h3>Put your email address below</h3>
-      <form method="POST" action="">
-        <input type="text" value={emailInput} onChange={(e) => setEmailInput(e.target.value)}></input>
-        <h3>And add all the stocks you want to subscribe to below</h3>
-        Tickers: {JSON.stringify(tickers)}
-        <input type="text" value={tickerInput} onChange={(e) => setTickerInput(e.target.value)}></input>
-        <button type="button" style={{ marginLeft: '5px' }} onClick={addTicker}>Add</button>
-        <br />
-        <br />
-        <br />
-        <h3>And then submit using the button below.</h3>
-        <div>
-          <button type="button" onClick={submitForm}>Submit</button>
+    <div>
+      <div className="MainContent">
+        <ToastContainer />
+        <div style={{ minHeight: '90vh' }}>
+          <h1>Welcome to earningsemails.com!</h1>
+          <h2>What is it?</h2>
+          <p>I was sick of missing earnings for stocks I follow. So I created this site. It will autmatically send you 2 emails for each earnings report:</p>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ol style={{ textAlign: 'left', maxWidth: '500px' }}>
+              <strong>
+                <li>One email when the company announces when they'll report their next earnings, so you can put it in your calendar. Or ignore it. I don't care.</li>
+                <li>And another one which contains the earnings results as soon as they're out.</li>
+              </strong>
+            </ol>
+          </div>
+          <h2>How does it work?</h2>
+          <h3>Put your email address below</h3>
+          <form method="POST" action="">
+            <input type="text" value={emailInput} onChange={(e) => setEmailInput(e.target.value)}></input>
+            <h3>And add all the stocks you want to subscribe to below</h3>
+            Tickers: {JSON.stringify(tickers)}
+            <input type="text" value={tickerInput} onChange={(e) => setTickerInput(e.target.value)}></input>
+            <button type="button" style={{ marginLeft: '5px' }} onClick={addTicker}>Add</button>
+            <br />
+            <br />
+            <br />
+            <h3>And then submit using the button below.</h3>
+            <div>
+              <button type="button" onClick={submitForm}>Submit</button>
+            </div>
+          </form>
         </div>
-      </form>
-      <p>Privacy: your data is stored on Firestore, which is a thing by Google, so there's that. It's stored in the US and it'll be looked at my me and anyone I want to show it to. If you don't like that, don't sign up.</p>
+      </div>
+      <div className="Footer"><a href="/privacy.html">Privacy</a><a href="/disclaimers.html">Disclaimers</a></div>
     </div>
   );
 }
